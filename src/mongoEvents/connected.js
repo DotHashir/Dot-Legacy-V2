b2f3e6db0797c08905482f259dde5a2a36398756
DotHashir <saleemgamingyt@gmail.com>
2021-08-20 11:12:04 -0700

    added MongoDB connection

---
+++ src/mongoEvents/connected.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'connected',
+    async execute() {
+        console.log(`Dot-Legacy V2 is now connected to MongoDB database`);
+    },
+};
---
+++ src/mongoEvents/disconnected.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'disconnected',
+    async execute() {
+        console.log(`Dot-Legacy V2 is now disconnected to MongoDB database`);
+    },
+};
